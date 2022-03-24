df488b86f1c16423502b450f5358b7aa737c04f1
Trong Hieu <tronghieu60s@gmail.com>
2022-03-24 15:23:45 +0700

    update source

--- ajax/ajax_house.php
+++ ajax/ajax_house.php
@@ -12,18 +12,26 @@
 	foreach ($house_list as $key => $value) {
 		$output .= '<div class="col-md-3 col-6">
 		<div class="cover-content">
-		<a href="'.$value['tenkhongdauvi'].'">
+		<a class="image" href="'.$value['tenkhongdauvi'].'">
 			<img src="thumbs/710x300x1/upload/news/'.$value['photo'].'" alt="">
 		</a>
 		<a href="'.$value['tenkhongdauvi'].'">
-			<h6 class="tab-icon uppercase">
+			<h6>
 				'.$func->catchuoi($value["tenvi"],40).'
 			</h6>
 		</a>
 		<a href="'.$value['tenkhongdauvi'].'" class="des-content">
-			<p class="date-tab">'.date('d/m/Y',$value['ngaytao']).'</p>
-			<p class="des-tab">'.$func->catchuoi($value["motavi"],40).'</p>
+			<!-- <p class="date-tab">'.date('d/m/Y',$value['ngaytao']).'</p> -->
+			<p class="des-tab">'.$func->catchuoi($value["motavi"],40). '</p>
 		</a>
+		<div class="price">
+			<div>
+				<p>2.990.000đ</p>
+				<p class="price-discount">2.990.000đ</p>
+			</div>
+			<div class="discount">-10%</div>
+		</div>
+		<button class="buy">Liên hệ</button>
 		</div>
 	</div>';
 	}
